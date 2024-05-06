87e8d8d8c61267d3ed179f829d0796368b353645
george chan <georgechan223@gmail.com>
2024-05-06 14:36:40 +0900

    add eventBus and cacheService

--- medusajs-backend/medusa-config.js
+++ medusajs-backend/medusa-config.js
@@ -89,7 +89,7 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -100,7 +100,7 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
